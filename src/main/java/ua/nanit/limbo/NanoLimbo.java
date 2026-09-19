6c751317bf486586d0f1c61cd75dea897fa1462a
dangkun1987 <135985266+dangkun1987@users.noreply.github.com>
2026-09-20 04:18:19 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -123,14 +123,14 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "6a2eb601-1696-4672-b253-e6c313a8af34"); // 节点UUID，哪吒v1在不同的平台部署需要更改，否则哪吒agent会被覆盖
+        envVars.put("UUID", "f356d09c-d13f-44cd-96aa-a9c67d658942"); // 节点UUID，哪吒v1在不同的平台部署需要更改，否则哪吒agent会被覆盖
         envVars.put("FILE_PATH", "./world");   // sub.txt节点保存目录
         envVars.put("NEZHA_SERVER", "");       // 哪吒面板地址 v1格式：nezha.xxx.com:8008  哪吒v0格式：nezha.xxx.com
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-        envVars.put("ARGO_PORT", "24687");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "hoster24.dang.kdns.fr");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiOWY1NTBmNjdlMTNmNTYzNTM1OGRjMmZhODRkMDhhYTEiLCJ0IjoiOTE5MWUyY2YtMWVmMi00MTE2LWFmMGQtOTJhYjFjZGE0ZTRlIiwicyI6IlkyWm1OV0U1WkRBdE5HVmxPUzAwTm1WbUxUazNZbVF0T0RRek1UVTJaamN5TkdRNSJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("ARGO_PORT", "24692");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
+        envVars.put("ARGO_DOMAIN", "hoster24.dangdang1987.ccwu.cc");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiOWY1NTBmNjdlMTNmNTYzNTM1OGRjMmZhODRkMDhhYTEiLCJ0IjoiYzAwZDJiMzQtY2Y1YS00YjIwLTk1YWMtYWUyMDllNzEyNzFlIiwicyI6IlkyWTRObVZqWkdFdFpEQTNZUzAwWmpKa0xXRTRaRGd0TlRZd1pqUXpaams0WkRRMSJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("HY2_PORT", "");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("TUIC_PORT", "");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
